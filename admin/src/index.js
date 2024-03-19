e8b4f2e7192c34970c86295e196969ac12bce635
matt-newhall <matt@newhall.dev>
2024-03-19 15:12:45 +0000

    feat: add new route to generate and send CSVs

--- admin/src/index.js
+++ admin/src/index.js
@@ -19,6 +19,58 @@ app.get("/investments/:id", (req, res) => {
   })
 })
 
+const generateHoldingsCSV = (userHoldings, companyNames) => {
+  let csv = ""
+  for (const user of userHoldings) {
+    for (const holding of user.holdings) {
+      const value = user.investmentTotal * holding.investmentPercentage
+      const company = companyNames[holding.id]
+      csv += `${user.userId},${user.firstName},${user.lastName},${user.date},${company},${value}\n`
+    }
+  }
+  return csv
+}
+
+app.post("/investments/export", async (_, res) => {
+  // Fetch data from other services
+  const companiesRes = await fetch(`${config.financialCompaniesUrl}/companies`)
+  const companies = await companiesRes.json()
+
+  const userHoldingsRes = await fetch(`${config.investmentsServiceUrl}/investments`)
+  const userHoldings = await userHoldingsRes.json()
+
+  // Generate dictionary of company indices and company names
+  const companyNames = companies.reduce((acc, company) => {
+    acc[company.id] = company.name
+    return acc
+  }, {})
+
+  // Generate CSV data
+  const csv = generateHoldingsCSV(userHoldings, companyNames)
+
+  // Send POST request to Investments service
+  const options = {
+    url: `${config.investmentsServiceUrl}/investments/export`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({csv}),
+  }
+
+  request(options, (e) => {
+    if (e) {
+      console.error(e)
+      res.status(500).send("Internal Server Error")
+      return
+    }
+
+    // Return csv from route as text
+    res.setHeader("Content-Type", "text/csv")
+    res.send(csv)
+  })
+})
+
 app.listen(config.port, (err) => {
   if (err) {
     console.error("Error occurred starting the server", err)
